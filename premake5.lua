3194ded1893d8b8bd9e4f9b8bd7ac0a9fa4cd7ca
toki0328 <1300544580@qq.com>
2024-06-09 20:41:57 +0800

    premake构建系统

---
+++ premake5.lua
@@ -0,0 +1,123 @@
+workspace "Pigeon"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Pigeon"
+	location "Pigeon"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/spdlog-1.14.1-win64/include"
+	}
+
+	-- 指定库目录
+	libdirs 
+	{
+		"%{prj.name}/vendor/spdlog/spdlog-1.14.1-win64/lib"
+	}
+
+	-- 指定要链接的库
+	links
+	{
+		"spdlogd.lib"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.22621.0"
+
+		defines
+		{
+			"PG_BUILD_DLL",
+			"PG_PLATFORM_WINDOWS"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "PG_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PG_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "PG_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Pigeon/vendor/spdlog/spdlog-1.14.1-win64/include",
+		"Pigeon/src"
+	}
+
+	libdirs 
+	{
+		"Pigeon/vendor/spdlog/spdlog-1.14.1-win64/lib"
+	}
+
+	links
+	{
+		"Pigeon",
+		"spdlogd.lib"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.22621.0"
+
+		defines
+		{
+			"PG_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "PG_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PG_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "PG_DIST"
+		optimize "On"
